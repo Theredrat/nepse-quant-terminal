4c28586206bbb47b8756bd951539db7b4b821f95
Theredrat <redrat07@gmail.com>
2026-06-06 12:13:00 +0545

    Clean up backup files and temp scripts

--- ck.py
+++
@@ -1,7 +0,0 @@
-﻿import sqlite3
-conn = sqlite3.connect('nepse_market_data.db')
-dates = conn.execute('SELECT DISTINCT date FROM broker_activity ORDER BY date DESC LIMIT 5').fetchall()
-print('Latest dates in broker_activity:')
-for d in dates:
-    print(' ', d[0])
-conn.close()
